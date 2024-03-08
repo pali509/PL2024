e0a536a746094e23a84209cc82d5410fc361f4da
pali509 <pablcamp@ucm.es>
2024-03-08 19:42:44 +0100

    ahora si es ejecutable

---
+++ TINY_Sintactico_Combinado/src/Main.java
@@ -0,0 +1,20 @@
+import java.text.ParseException;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        try{
+        /*
+        1.Pedir archivo
+        2.Pedir opcion
+        3. llamar con la opcion elegida a la clase Main de el que toque con e larchivo seleccionado
+         */
+        }
+        catch(ParseException e) {
+            System.out.println("ERROR_SINTACTICO");
+        }
+        catch(asint.TokenMgrError e) {
+            System.out.println("ERROR_LEXICO");
+        }
+    }
+    }
+}
