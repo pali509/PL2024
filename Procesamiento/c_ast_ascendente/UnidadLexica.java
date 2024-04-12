57d1704924065a6e1424d5b2a0baeeb4ad2550ce
pali509 <pablcamp@ucm.es>
2024-04-12 21:04:15 +0200

    aaa

--- Procesamiento/c_ast_ascendente/UnidadLexica.java
+++ Procesamiento/c_ast_ascendente/UnidadLexica.java
@@ -6,18 +6,18 @@ public class UnidadLexica extends Symbol {
    public static class StringLocalizado {
      private int beginLine;
      private int beginColumn;
-     private String image;
-     public StringLocalizado(String s, int fila, int col) {
-         this.image = s;
-         this.beginLine = fila;
-         this.beginColumn = col;  
+     private String s;
+     public StringLocalizado(String s, int beginLine, int beginColumn) {
+         this.s = s;
+         this.beginLine = beginLine;
+         this.beginColumn = beginColumn;
      }
      public int beginLine() {return beginLine;}
      public int beginColumn() {return beginColumn;}
-     public String image() {return image;}
+     public String image() {return s;}
    }
-   public UnidadLexica(int fila, int columna, int clase, String lexema) {
-     super(clase, new StringLocalizado(lexema,fila,columna));  
+   public UnidadLexica(int beginLine, int beginbeginColumnumn, int clase, String lexema) {
+     super(clase, new StringLocalizado(lexema,beginLine,beginbeginColumnumn));
    }
    public int clase () {return sym;}
    public int fila() {return ((StringLocalizado)value).beginLine();}
--- Procesamiento/c_ast_ascendente/asint/ClaseSemanticaTiny.java
+++ Procesamiento/c_ast_ascendente/asint/ClaseSemanticaTiny.java
@@ -1,6 +1,8 @@
 package c_ast_ascendente.asint;
 
 
+import c_ast_ascendente.UnidadLexica;
+
 public class ClaseSemanticaTiny extends SintaxisAbstractaTiny {
     public ClaseSemanticaTiny() {
         super();
@@ -29,5 +31,8 @@ public class ClaseSemanticaTiny extends SintaxisAbstractaTiny {
             case "not": return not(opnd);
             default: throw new UnsupportedOperationException("Bad op");
         }
+    }
+    public UnidadLexica.StringLocalizado StringLocalizado(String s, int fila, int col) {
+        return new UnidadLexica.StringLocalizado(s, fila, col);
     }
 }
