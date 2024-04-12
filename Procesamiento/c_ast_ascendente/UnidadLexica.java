ed65f26ac7d0ae7ed4a873886585220a23dde3dd
destherhada <48451168+destherhada@users.noreply.github.com>
2024-04-12 21:22:05 +0200

    Add files via upload

--- Procesamiento/c_ast_ascendente/UnidadLexica.java
+++ Procesamiento/c_ast_ascendente/UnidadLexica.java
@@ -3,24 +3,27 @@ package c_ast_ascendente;
 import java_cup.runtime.Symbol;
 
 public class UnidadLexica extends Symbol {
-   public static class StringLocalizado {
-     private int beginLine;
-     private int beginColumn;
-     private String s;
-     public StringLocalizado(String s, int beginLine, int beginColumn) {
-         this.s = s;
-         this.beginLine = beginLine;
-         this.beginColumn = beginColumn;
-     }
-     public int beginLine() {return beginLine;}
-     public int beginColumn() {return beginColumn;}
-     public String image() {return s;}
+
+	public static class StringLocalizado {
+       private String image;
+       private int beginLine;
+       private int beginColumn;
+       public StringLocalizado(String s, int fila, int col) {
+           this.image = s;
+           this.beginLine = fila;
+           this.beginColumn = col;
+       }
+       public int beginLine() {return beginLine;}
+       public int beginColumn() {return beginColumn;}
+       public String toString() {
+           return image;
+       }
    }
-   public UnidadLexica(int beginLine, int beginbeginColumnumn, int clase, String lexema) {
-     super(clase, new StringLocalizado(lexema,beginLine,beginbeginColumnumn));
+   public UnidadLexica(int fila, int columna, int clase, String lexema) {
+     super(clase, new StringLocalizado(lexema,fila,columna));  
    }
    public int clase () {return sym;}
-   public int fila() {return ((StringLocalizado)value).beginLine();}
-   public int columna() {return ((StringLocalizado)value).beginColumn();}
-   public String lexema() {return ((StringLocalizado)value).image();}
+   public int fila() {return ((StringLocalizado)value).fila();}
+   public int columna() {return ((StringLocalizado)value).col();}
+   public String lexema() {return ((StringLocalizado)value).str();}
 }
