6858f5f274871ad507bdcb866c5c93edd88bab94
destherhada <48451168+destherhada@users.noreply.github.com>
2024-05-05 22:05:01 +0200

    todo Exp hecho

--- Fase4/src/procesamientos/Gen_cod.java
+++ Fase4/src/procesamientos/Gen_cod.java
@@ -125,120 +125,531 @@ public class Gen_cod extends ProcesamientoDef {
         u.e().procesa(this);
     }
 
+    
+    
+    
+    //GEN-COD EXPRESIONES
+    
     public void procesa(Exp_lit_ent e){
-
+    	mp.apila_int(Integer.parseInt(e.valor().toString()));
     }
 
     public void procesa(Exp_lit_real e){
-
+    	mp.apila_real(Double.parseDouble(e.valor().toString()));
     }
 
     public void procesa(Exp_lit_cadena e){
-
+    	mp.apila_string(e.valor().toString());
     }
 
     public void procesa(Exp_Iden e){
-
+    	/*
+    	if(e.getVinculo().getNivel() != 0) {
+    		mp.apilad(e.getVinculo().getNivel());
+    		mp.apila_int(e.getVinculo().getDir());
+    		mp.suma_int();
+    		if(e.getVinculo().es_parf_ref()){
+    			mp.apila_int();
+    	}
+    	else{
+    		mp.apila_int(e.getVinculo().getDir());
+    	
+    	*/
     }
 
     public void procesa(Exp_lit_BoolTrue e){
-
+    	mp.apila_bool(true);
     }
 
     public void procesa(Exp_lit_BoolFalse e){
-
+    	mp.apila_bool(false);
     }
 
     public void procesa(Exp_null e){
-
+    	mp.apila_int(-1);
     }
 
     public void procesa(Asig a){
-
+    	a.opnd0().procesa(this);
+    	a.opnd1().procesa(this);
+    	if(refI(a.opnd1().tipo()).es_int() && refI(a.opnd0().tipo()).es_real()) {
+    		if(es_desig(a.opnd1())) {
+    			mp.apila_ind();
+    		}
+    		mp.int2real();
+    		mp.desapila_ind();
+    	}
+    	else {
+    		if(es_desig(a.opnd1())) {
+    			mp.mueve(refI(a.opnd0().tipo()).getTam());
+    		}
+    		else {
+    			mp.desapila_ind();
+    		}
+    	}
+    	
     }
 
     public void procesa(Suma s){
+    	Tipo t0 = refI(s.opnd0().tipo());
+    	Tipo t1 = refI(s.opnd1().tipo());
+    	s.opnd0().procesa(this);
+    	if(es_desig(s.opnd0())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_int() && t1.es_real()) {
+    		mp.int2real();
+    	}
+    	s.opnd1().procesa(this);
+    	if(es_desig(s.opnd1())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_real() && t1.es_int()) {
+    		mp.int2real();
+    	}
+    	if(t0.es_int() && t1.es_int()) {
+    		mp.suma_int();
+    	}
+    	else if(t0.es_real() && t1.es_real()) {
+    		mp.suma_real();
+    	}
+  
+    	
+    }
+
+    public void procesa(Resta s){
+    	Tipo t0 = refI(s.opnd0().tipo());
+    	Tipo t1 = refI(s.opnd1().tipo());
+    	s.opnd0().procesa(this);
+    	if(es_desig(s.opnd0())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_int() && t1.es_real()) {
+    		mp.int2real();
+    	}
+    	s.opnd1().procesa(this);
+    	if(es_desig(s.opnd1())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_real() && t1.es_int()) {
+    		mp.int2real();
+    	}
+    	if(t0.es_int() && t1.es_int()) {
+    		mp.resta_int();
+    	}
+    	else if(t0.es_real() && t1.es_real()) {
+    		mp.resta_real();
+    	}
+    }
+
+    public void procesa(Mul s){
+    	Tipo t0 = refI(s.opnd0().tipo());
+    	Tipo t1 = refI(s.opnd1().tipo());
+    	s.opnd0().procesa(this);
+    	if(es_desig(s.opnd0())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_int() && t1.es_real()) {
+    		mp.int2real();
+    	}
+    	s.opnd1().procesa(this);
+    	if(es_desig(s.opnd1())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_real() && t1.es_int()) {
+    		mp.int2real();
+    	}
+    	if(t0.es_int() && t1.es_int()) {
+    		mp.mul_int();
+    	}
+    	else if(t0.es_real() && t1.es_real()) {
+    		mp.mul_real();
+    	}
+    }
+
+    public void procesa(Div s){
+    	Tipo t0 = refI(s.opnd0().tipo());
+    	Tipo t1 = refI(s.opnd1().tipo());
+    	s.opnd0().procesa(this);
+    	if(es_desig(s.opnd0())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_int() && t1.es_real()) {
+    		mp.int2real();
+    	}
+    	s.opnd1().procesa(this);
+    	if(es_desig(s.opnd1())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_real() && t1.es_int()) {
+    		mp.int2real();
+    	}
+    	if(t0.es_int() && t1.es_int()) {
+    		mp.div_int();
+    	}
+    	else if(t0.es_real() && t1.es_real()) {
+    		mp.div_real();
+    	}
+    }
+
+    public void procesa(Mod s){
+    	Tipo t0 = refI(s.opnd0().tipo());
+    	Tipo t1 = refI(s.opnd1().tipo());
+    	s.opnd0().procesa(this);
+    	if(es_desig(s.opnd0())) {
+    		mp.apila_ind();
+    	}
+
+    	s.opnd1().procesa(this);
+    	if(es_desig(s.opnd1())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_int() && t1.es_int()) {
+    		mp.mod();
+    	}
 
     }
 
-    public void procesa(Resta r){
+    public void procesa(And s){
+    	Tipo t0 = refI(s.opnd0().tipo());
+    	Tipo t1 = refI(s.opnd1().tipo());
+    	s.opnd0().procesa(this);
+    	if(es_desig(s.opnd0())) {
+    		mp.apila_ind();
+    	}
+
+    	s.opnd1().procesa(this);
+    	if(es_desig(s.opnd1())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_bool() && t1.es_bool()) {
+    		mp.and();
+    	}
+    }
+
+    public void procesa(Or s){
+    	Tipo t0 = refI(s.opnd0().tipo());
+    	Tipo t1 = refI(s.opnd1().tipo());
+    	s.opnd0().procesa(this);
+    	if(es_desig(s.opnd0())) {
+    		mp.apila_ind();
+    	}
+
+    	s.opnd1().procesa(this);
+    	if(es_desig(s.opnd1())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_bool() && t1.es_bool()) {
+    		mp.or();
+    	}
+    }
+    
+
+    public void procesa(Mayor s){
+    	Tipo t0 = refI(s.opnd0().tipo());
+    	Tipo t1 = refI(s.opnd1().tipo());
+    	s.opnd0().procesa(this);
+    	if(es_desig(s.opnd0())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_int() && t1.es_real()) {
+    		mp.int2real();
+    	}
+    	s.opnd1().procesa(this);
+    	if(es_desig(s.opnd1())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_real() && t1.es_int()) {
+    		mp.int2real();
+    	}
+    	if(t0 == t1) {
+    		if(t0.es_real()) {
+    			mp.mayor_real();
+    			}
+    		else if(t0.es_int()) {
+    			mp.mayor_int();
+    		}
+    		else if(t0.es_bool()) {
+    			mp.mayor_bool();
+    		}
+    		else if(t0.es_string()){
+    			mp.mayor_string();
+    		}
+    	}
 
     }
 
-    public void procesa(Mul m){
-
+    public void procesa(Menor s){
+    	Tipo t0 = refI(s.opnd0().tipo());
+    	Tipo t1 = refI(s.opnd1().tipo());
+    	s.opnd0().procesa(this);
+    	if(es_desig(s.opnd0())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_int() && t1.es_real()) {
+    		mp.int2real();
+    	}
+    	s.opnd1().procesa(this);
+    	if(es_desig(s.opnd1())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_real() && t1.es_int()) {
+    		mp.int2real();
+    	}
+    	if(t0 == t1) {
+    		if(t0.es_real()) {
+    			mp.menor_real();
+    			}
+    		else if(t0.es_int()) {
+    			mp.menor_int();
+    		}
+    		else if(t0.es_bool()) {
+    			mp.menor_bool();
+    		}
+    		else if(t0.es_string()){
+    			mp.menor_string();
+    		}
+    	}
     }
 
-    public void procesa(Div d){
-
+    public void procesa(MayorIg s){
+    	Tipo t0 = refI(s.opnd0().tipo());
+    	Tipo t1 = refI(s.opnd1().tipo());
+    	s.opnd0().procesa(this);
+    	if(es_desig(s.opnd0())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_int() && t1.es_real()) {
+    		mp.int2real();
+    	}
+    	s.opnd1().procesa(this);
+    	if(es_desig(s.opnd1())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_real() && t1.es_int()) {
+    		mp.int2real();
+    	}
+    	if(t0 == t1) {
+    		if(t0.es_real()) {
+    			mp.mayorIgual_real();
+    			}
+    		else if(t0.es_int()) {
+    			mp.mayorIgual_int();
+    		}
+    		else if(t0.es_bool()) {
+    			mp.mayorIgual_bool();
+    		}
+    		else if(t0.es_string()){
+    			mp.mayorIgual_string();
+    		}
+    	}
     }
 
-    public void procesa(Mod m){
-
+    public void procesa(MenorIg s){
+    	Tipo t0 = refI(s.opnd0().tipo());
+    	Tipo t1 = refI(s.opnd1().tipo());
+    	s.opnd0().procesa(this);
+    	if(es_desig(s.opnd0())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_int() && t1.es_real()) {
+    		mp.int2real();
+    	}
+    	s.opnd1().procesa(this);
+    	if(es_desig(s.opnd1())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_real() && t1.es_int()) {
+    		mp.int2real();
+    	}
+    	if(t0 == t1) {
+    		if(t0.es_real()) {
+    			mp.menorIgual_real();
+    			}
+    		else if(t0.es_int()) {
+    			mp.menorIgual_int();
+    		}
+    		else if(t0.es_bool()) {
+    			mp.menorIgual_bool();
+    		}
+    		else if(t0.es_string()){
+    			mp.menorIgual_string();
+    		}
+    	}
     }
 
-    public void procesa(And a){
-
+    public void procesa(Igual s){
+    	Tipo t0 = refI(s.opnd0().tipo());
+    	Tipo t1 = refI(s.opnd1().tipo());
+    	s.opnd0().procesa(this);
+    	if(es_desig(s.opnd0())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_int() && t1.es_real()) {
+    		mp.int2real();
+    	}
+    	s.opnd1().procesa(this);
+    	if(es_desig(s.opnd1())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_real() && t1.es_int()) {
+    		mp.int2real();
+    	}
+    	if(t0 == t1) {
+    		if(t0.es_real()) {
+    			mp.igual_real();
+    			}
+    		else if(t0.es_int()) {
+    			mp.igual_int();
+    		}
+    		else if(t0.es_bool()) {
+    			mp.igual_bool();
+    		}
+    		else if(t0.es_string()){
+    			mp.igual_string();
+    		}
+    		else if(t0.es_puntero()) {
+    			mp.igual_puntero();
+    		}
+    		else if(t0.es_null()) {
+    			mp.igual_null();
+    		}
+    	}
+    	else if(t0.es_puntero() && t1.es_null() || t0.es_null() && t1.es_puntero()) {
+    		mp.igual_raro();
+    	}
     }
 
-    public void procesa(Or o){
-
+    public void procesa(Desigual s){
+       	Tipo t0 = refI(s.opnd0().tipo());
+    	Tipo t1 = refI(s.opnd1().tipo());
+    	s.opnd0().procesa(this);
+    	if(es_desig(s.opnd0())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_int() && t1.es_real()) {
+    		mp.int2real();
+    	}
+    	s.opnd1().procesa(this);
+    	if(es_desig(s.opnd1())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_real() && t1.es_int()) {
+    		mp.int2real();
+    	}
+    	if(t0 == t1) {
+    		if(t0.es_real()) {
+    			mp.desigual_real();
+    			}
+    		else if(t0.es_int()) {
+    			mp.desigual_int();
+    		}
+    		else if(t0.es_bool()) {
+    			mp.desigual_bool();
+    		}
+    		else if(t0.es_string()){
+    			mp.desigual_string();
+    		}
+    		else if(t0.es_puntero()) {
+    			mp.desigual_puntero();
+    		}
+    		else if(t0.es_null()) {
+    			mp.desigual_null();
+    		}
+    	}
+    	else if(t0.es_puntero() && t1.es_null() || t0.es_null() && t1.es_puntero()) {
+    		mp.desigual_raro();
+    	}
     }
 
-    public void procesa(Mayor ma){
+    public void procesa(Not n){
+    	Tipo t0 = refI(n.opnd0().tipo());
+    	n.opnd0().procesa(this);
+    	if(es_desig(n.opnd0())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_bool()) {
+    		mp.not();
+    	}
 
+    	
+    }
+    
+    public void procesa(Neg n) {
+    	Tipo t0 = refI(n.opnd0().tipo());
+    	n.opnd0().procesa(this);
+    	if(es_desig(n.opnd0())) {
+    		mp.apila_ind();
+    	}
+    	if(t0.es_int()) {
+    		mp.neg_int();
+    	}
+    	else if(t0.es_real()) {
+    		mp.neg_real();
+    	}
     }
 
-    public void procesa(Menor me){
+    public void procesa(Lit_bool l){}
 
+    public void procesa(Lit_ent l){}
+
+    public void procesa(Lit_string l){}
+
+    public void procesa(Lit_real l){}
+
+    public void procesa(Iden i){}
+    
+    public void procesa(AccesoCampo s){
+    	s.opnd0().procesa(this);
+    	mp.apila_int(desp(refI(s.opnd0().tipo()).lcamp(), s.iden().toString()));
+    	mp.suma_int();
+    }
+    public void procesa(AccesoArray a){
+        a.opnd0().procesa(this);
+        a.opnd1().procesa(this);
+        if(es_desig(a.opnd1())) {
+        	mp.apila_ind();
+        }
+        mp.apila_int(refI(a.opnd0().tipo()).tipo().getTam());
+        mp.mul_int();
+        mp.suma_int();
+        
+    }
+    public void procesa(AccesoPuntero p) {
+    	p.opnd0().procesa(this);
+    	mp.apila_ind();
+    }
+    
+    
+    
+    //FUNCIONES AUX PARA EXP
+    private Tipo refI(Tipo t) {
+    	while(t.es_iden()) {
+    		Iden id = (Iden)t;
+    		Dec_tipo d = (Dec_tipo) id.getVinculo();
+    		t = d.tipo();
+    	}
+    	return t;
+    }
+    
+    private boolean es_desig(Exp e) {
+    	boolean a = false;
+    	if(e.getVinculo() != null) {
+    		a = true;
+    	}
+    	return a;
+    }
+    
+    private int desp(LCamp lcs, String c) {
+    	while(lcs.es_muchos_campos()) {
+    		Camp campo = lcs.campo();
+    		if(c.equals(campo.iden().toString())) {
+    			return campo.get_desp();
+    		}
+    		lcs = lcs.lcs();
+    	}
+    	Camp campo = lcs.campo();
+    	return campo.get_desp();
     }
 
-    public void procesa(MayorIg mai){
-
-    }
-
-    public void procesa(MenorIg mei){
-
-    }
-
-    public void procesa(Igual ig){
-
-    }
-
-    public void procesa(Desigual de){
-
-    }
-
-    public void procesa(Not no){
-
-    }
-
-    public void procesa(Lit_bool l){
-        //NOOP
-    }
-
-    public void procesa(Lit_ent l){
-        //NOOP
-    }
-
-    public void procesa(Lit_string l){
-        //NOOP
-    }
-
-    public void procesa(Lit_real l){
-        //NOOP
-    }
-
-    public void procesa(Iden i){
-        //NOOP
-    }
-    public void procesa(Struct s){
-        s.lcamp().procesa(this);
-    }
-    public void procesa(Array a){
-        a.tipo().procesa(this);
-    }
-    public void procesa(Puntero p) {
-
-    }
 }
