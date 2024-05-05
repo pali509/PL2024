c07e06a47c4bdbcb4bbec397853605866e311c66
pali509 <pablcamp@ucm.es>
2024-05-05 11:12:21 +0200

    añadido maquinap

--- Fase4/src/procesamientos/Gen_cod.java
+++ Fase4/src/procesamientos/Gen_cod.java
@@ -1,4 +1,17 @@
 package procesamientos;
+import asint.SintaxisAbstractaTiny.*;
+import asint.ProcesamientoDef;
+import java.util.Stack;
+import maquinaP.*;
 
 public class Gen_cod {
+    private MaquinaP mp;
+
+    public Generacion_codigo(MaquinaP mp) {
+        this.mp = mp;
+    }
+
+    private Stack<Dec_proc> procs = new Stack<Dec_proc>();
+
+
 }
