55e9e696d1238ba59949746933caeac114ee51db
destherhada <48451168+destherhada@users.noreply.github.com>
2024-05-07 21:06:57 +0200

    Add files via upload

--- Fase4/src/procesamientos/MensajesError.java
+++ Fase4/src/procesamientos/MensajesError.java
@@ -7,24 +7,27 @@ public class MensajesError {
 
     //PROPUESTA DE USO: 
     //en cada procesamiento 1. creo una instancia de esta clase con un booleano que indica si hay errores
-    //                      2. voy aÃ±adiendo los errores a la instancia que he creado
+    //                      2. voy añadiendo los errores a la instancia que he creado
     //                      3. Si el booleano == true --> ordeno a la instancia que imprima y termino la ejecucion
     //                      4. Si el booleano == false --> no imprimo nada y voy al siguiente procesamiento
 
     private boolean hayError;
-    private String procesamiento;
     private List<Integer> listaFilas;
     private List<Integer> listaCols;
+    private String tipo;
 
-    public MensajesError(String procesamiento){
+    public MensajesError(){
         hayError = false;
-        this.procesamiento = "Errores_" + procesamiento;
         listaFilas = new ArrayList<>();
         listaCols = new ArrayList<>();
     }
     
     public boolean getHayError(){
         return hayError;
+    }
+    
+    public void setTipoError(String e) {
+    	this.tipo = "Errores_" + e;
     }
 
     public void addError(int f, int c){
@@ -35,7 +38,7 @@ public class MensajesError {
     public void getErrores(){
         //ordenaErrores();
         for (int i = 0; i < listaFilas.size(); i++) {
-            System.out.println(procesamiento + " fila:" + listaFilas.get(i) + " col:" + listaCols.get(i));
+            System.out.println(tipo + " fila:" + listaFilas.get(i) + " col:" + listaCols.get(i));
         }
     }
 
