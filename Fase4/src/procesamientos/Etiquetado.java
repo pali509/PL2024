9105587b51a0f52b46013919bad0a8b8eb658b2c
José Antonio Ruiz Heredia <56451051+JoseRuiz01@users.noreply.github.com>
2024-05-10 15:31:29 +0200

    Cambiado etiquetado

--- Fase4/src/procesamientos/Etiquetado.java
+++ Fase4/src/procesamientos/Etiquetado.java
@@ -3,15 +3,17 @@ package procesamientos;
 import java.util.Stack;
 import asint.ProcesamientoDef;
 import asint.SintaxisAbstractaTiny.*;
+import maquinaP.MaquinaP;
 
 public class Etiquetado extends ProcesamientoDef {
+    private MaquinaP mp;
+
+    public Etiquetado(MaquinaP mp) {
+        this.mp = mp;
+    }
 
     private Stack<Dec_proc> procs = new Stack<Dec_proc>();
     private int etq;
-
-    private boolean es_designador(Exp e){
-        return true;
-    }
 
     public void procesa(Prog p){
         etq = 0;
@@ -22,32 +24,11 @@ public class Etiquetado extends ProcesamientoDef {
         // b.prim = etq;
         etq++;
         // b.sig = etq;
-        recolecta_procs(b.lds());
+        b.lds().procesa(this);
         b.lis().procesa(this);
         while (!procs.isEmpty()) {
             Dec_proc proc = procs.pop();
             proc.procesa(this);
-        }
-    }
-
-    public void recolecta_procs(LDecsOpt d){
-        if(d.es_si_decs()) {
-            Si_decs dnew = (Si_decs) d;
-            recolecta_procs(dnew);
-        }
-        else if(d.es_no_decs()) {
-            No_decs dnew = (No_decs) d;
-            recolecta_procs(dnew);
-        }
-    }
-    public void recolecta_procs(Si_decs s){ //TODO
-        if(s.es_muchas_decs()) {
-            Muchas_decs dnew = (Muchas_decs) s;
-            recolecta_procs(dnew);
-        }
-        else if(s.es_una_dec()) {
-            Una_dec dnew = (Una_dec) s;
-            recolecta_procs(dnew);
         }
     }
 
@@ -68,8 +49,17 @@ public class Etiquetado extends ProcesamientoDef {
         u.dec().procesa(this);
     }
 
-    public void procesa(Dec_proc d){
-        procs.push(d);
+    public void procesa(Dec d){
+        if(d.es_dec_proc()) {
+            Dec_proc dnew = (Dec_proc) d;
+
+            //TODO Es o eso o hacer otro recolecta para hacer mp.emit(apila)
+            procs.push(dnew);
+        }
+    }
+
+    public void procesa(Dec_proc dec_proc){
+        dec_proc.bq().procesa(this);
     }
 
     public void procesa(Si_Ins s){
@@ -81,8 +71,10 @@ public class Etiquetado extends ProcesamientoDef {
     }
 
     public void procesa(Muchas_ins m){
+        // b.prim = etq;
         m.ins().procesa(this);
         m.li().procesa(this);
+        // b.sig = etq;
     }
 
     public void procesa(Una_ins u){
@@ -94,14 +86,25 @@ public class Etiquetado extends ProcesamientoDef {
     }
 
     public void procesa(Ins_if i){
+        i.prim = etq;
         i.e().procesa(this);
+        if(es_desig(i.e()))
+            etq++;
+        mp.emit(mp.ir_f(i.sig));
         i.bloque().procesa(this);
+        i.sig = etq;
     }
 
     public void procesa(Ins_if_else i){
+        i.prim = etq;
         i.e().procesa(this);
+        if(es_desig(i.e()))
+            etq++;
+        mp.emit(mp.ir_f(i.bloque2().prim()));
         i.bloque().procesa(this);
+        mp.emit(mp.ir_f(i.sig()));
         i.bloque2().procesa(this);
+        i.sig = etq;
     }
 
     public void procesa(Ins_call i){
@@ -113,334 +116,368 @@ public class Etiquetado extends ProcesamientoDef {
         i.bloque().procesa(this);
     }
 
-    public void procesa(Ins_read i){
+    public void procesa(Ins_read i) {
+        i.prim = etq;
         i.e().procesa(this);
+        Tipo t = refI(i.e().tipo());
+        if (t.es_int() || t.es_real() || t.es_string()) {
+            etq++;
+            mp.emit(mp.store(i.e().procesa(this), t));
+        }
+        i.sig = etq;
     }
 
-    public void procesa(Ins_write i){
+    public void procesa (Ins_write i){
+        i.prim = etq;
         i.e().procesa(this);
+        i.sig = etq;
     }
 
-    public void procesa(Ins_nl i){
+    public void procesa (Ins_nl i){
         //NOOP
     }
 
-    public void procesa(Ins_new i){
+    public void procesa (Ins_new i){
+        i.prim = etq;
         i.e().procesa(this);
+        i.sig = etq;
     }
 
-    public void procesa(Ins_delete i){
+    public void procesa (Ins_delete i){
+        i.prim = etq;
         i.e().procesa(this);
+        i.sig = etq;
     }
 
-    public void procesa(Si_preal s){
+    public void procesa(Ins_bloque i){
+        i.prim = etq;
+        i.e().procesa(this);
+        i.sig = etq;
+    }
+    public void procesa (Si_preal s){
         s.lpr().procesa(this);
     }
 
-    public void procesa(No_preal n){
+    public void procesa (No_preal n){
         //NOOP
     }
 
-    public void procesa(Muchos_preal m){
+    public void procesa (Muchos_preal m){
         m.lpr().procesa(this);
         m.e().procesa(this);
     }
 
-    public void procesa(Un_PReal u){
+    public void procesa (Un_PReal u){
         u.e().procesa(this);
     }
 
-    public void procesa(Exp_lit_ent e){
+    // ETIQUETADO EXPRESIONES
+    public void procesa (Exp_lit_ent e){
         etq++;
     }
 
-    public void procesa(Exp_lit_real e){
+    public void procesa (Exp_lit_real e){
         etq++;
     }
 
-    public void procesa(Exp_lit_cadena e){
+    public void procesa (Exp_lit_cadena e){
         etq++;
     }
 
-    public void procesa(Exp_Iden e){
+    public void procesa (Exp_Iden e){
         etq++;
     }
 
-    public void procesa(Exp_lit_BoolTrue e){
+    public void procesa (Exp_lit_BoolTrue e){
         etq++;
     }
 
-    public void procesa(Exp_lit_BoolFalse e){
+    public void procesa (Exp_lit_BoolFalse e){
         etq++;
     }
 
-    public void procesa(Exp_null e){
+    public void procesa (Exp_null e){
         etq++;
     }
 
-    public void procesa(Asig a){
+    public void procesa (Asig a){
         a.opnd0().procesa(this);
         a.opnd1().procesa(this);
         etq++;
         String t0 = a.opnd0().tipo().iden().toString();
         String t1 = a.opnd1().tipo().iden().toString();
-        if(t0.equals("real") && t1.equals("int")) {
-            if(es_designador(a.opnd1())) {
+        if(refI(a.opnd1().tipo()).es_int() && refI(a.opnd0().tipo()).es_real()) {
+            if (es_desig(a.opnd1())) {
                 etq++;
             }
         } else {
-            if(es_designador(a.opnd1())) {
+            if (es_desig(a.opnd1())) {
                 etq++;
             }
         }
     }
 
-    public void procesa(Suma s){
+    public void procesa (Suma s){
         s.opnd0().procesa(this);
         s.opnd1().procesa(this);
         etq++;
-        String t0 = s.opnd0().tipo().iden().toString();
-        String t1 = s.opnd1().tipo().iden().toString();
-        if ((t0.equals("int") && t1.equals("real")) || (t0.equals("real") && t1.equals("int"))) {
-            if (es_designador(s.opnd0())) {
+        if ((refI(s.opnd0().tipo()).es_int() || refI(s.opnd0().tipo()).es_real()) &&
+                (refI(s.opnd1().tipo()).es_int() || refI(s.opnd1().tipo()).es_real())) {
+            if (es_desig(s.opnd0())) {
                 etq++;
             }
-            if (es_designador(s.opnd1())) {
+            if (es_desig(s.opnd1())) {
                 etq++;
             }
         }
     }
 
-    public void procesa(Resta r){
+    public void procesa (Resta r){
         r.opnd0().procesa(this);
         r.opnd1().procesa(this);
         etq++;
-        String t0 = r.opnd0().tipo().iden().toString();
-        String t1 = r.opnd1().tipo().iden().toString();
-        if ((t0.equals("int") && t1.equals("real")) || (t0.equals("real") && t1.equals("int"))) {
-            if (es_designador(r.opnd0())) {
+        if ((refI(r.opnd0().tipo()).es_int() || refI(r.opnd0().tipo()).es_real()) &&
+                (refI(r.opnd1().tipo()).es_int() || refI(r.opnd1().tipo()).es_real())) {
+            if (es_desig(r.opnd0())) {
                 etq++;
             }
-            if (es_designador(r.opnd1())) {
+            if (es_desig(r.opnd1())) {
                 etq++;
             }
         }
     }
 
-    public void procesa(Mul m){
+    public void procesa (Mul m){
         m.opnd0().procesa(this);
         m.opnd1().procesa(this);
         etq++;
-        String t0 = m.opnd0().tipo().iden().toString();
-        String t1 = m.opnd1().tipo().iden().toString();
-        if ((t0.equals("int") && t1.equals("real")) || (t0.equals("real") && t1.equals("int"))) {
-            if (es_designador(m.opnd0())) {
+        if ((refI(m.opnd0().tipo()).es_int() || refI(m.opnd0().tipo()).es_real()) &&
+                (refI(m.opnd1().tipo()).es_int() || refI(m.opnd1().tipo()).es_real())) {
+            if (es_desig(m.opnd0())) {
                 etq++;
             }
-            if (es_designador(m.opnd1())) {
+            if (es_desig(m.opnd1())) {
                 etq++;
             }
         }
     }
 
-    public void procesa(Div d){
+    public void procesa (Div d){
         d.opnd0().procesa(this);
         d.opnd1().procesa(this);
         etq++;
-        String t0 = d.opnd0().tipo().iden().toString();
-        String t1 = d.opnd1().tipo().iden().toString();
-        if ((t0.equals("int") && t1.equals("real")) || (t0.equals("real") && t1.equals("int"))) {
-            if (es_designador(d.opnd0())) {
+        if ((refI(d.opnd0().tipo()).es_int() || refI(d.opnd0().tipo()).es_real()) &&
+                (refI(d.opnd1().tipo()).es_int() || refI(d.opnd1().tipo()).es_real())) {
+            if (es_desig(d.opnd0())) {
                 etq++;
             }
-            if (es_designador(d.opnd1())) {
+            if (es_desig(d.opnd1())) {
                 etq++;
             }
         }
     }
 
-    public void procesa(Mod m){
+    public void procesa (Mod m){
         m.opnd0().procesa(this);
         m.opnd1().procesa(this);
         etq++;
-        String t0 = m.opnd0().tipo().iden().toString();
-        String t1 = m.opnd1().tipo().iden().toString();
-        if ((t0.equals("int") && t1.equals("int"))) {
-            if (es_designador(m.opnd0())) {
+        if (refI(m.opnd0().tipo()).es_int() && refI(m.opnd1().tipo()).es_int()) {
+            if (es_desig(m.opnd0())) {
                 etq++;
             }
-            if (es_designador(m.opnd1())) {
+            if (es_desig(m.opnd1())) {
                 etq++;
             }
         }
     }
 
-    public void procesa(And a){
+    public void procesa (And a){
         a.opnd0().procesa(this);
         a.opnd1().procesa(this);
         etq++;
-        String t0 = a.opnd0().tipo().iden().toString();
-        String t1 = a.opnd1().tipo().iden().toString();
-        if ((t0.equals("bool") && t1.equals("bool"))) {
-            if (es_designador(a.opnd0())) {
+        if (refI(a.opnd0().tipo()).es_bool() && refI(a.opnd1().tipo()).es_bool()) {
+            if (es_desig(a.opnd0())) {
                 etq++;
             }
-            if (es_designador(a.opnd1())) {
+            if (es_desig(a.opnd1())) {
                 etq++;
             }
         }
     }
 
-    public void procesa(Or o){
+    public void procesa (Or o){
         o.opnd0().procesa(this);
         o.opnd1().procesa(this);
         etq++;
-        String t0 = o.opnd0().tipo().iden().toString();
-        String t1 = o.opnd1().tipo().iden().toString();
-        if ((t0.equals("bool") && t1.equals("bool"))) {
-            if (es_designador(o.opnd0())) {
+        if (refI(o.opnd0().tipo()).es_bool() && refI(o.opnd1().tipo()).es_bool()) {
+            if (es_desig(o.opnd0())) {
                 etq++;
             }
-            if (es_designador(o.opnd1())) {
+            if (es_desig(o.opnd1())) {
                 etq++;
             }
         }
     }
 
-    public void procesa(Mayor ma){
+    public void procesa (Mayor ma){
         ma.opnd0().procesa(this);
         ma.opnd1().procesa(this);
         etq++;
-        String t0 = ma.opnd0().tipo().iden().toString();
-        String t1 = ma.opnd1().tipo().iden().toString();
-        if ((t0.equals("int") && t1.equals("real"))) {
-            if (es_designador(ma.opnd0())) {
+        if ((refI(ma.opnd0().tipo()).es_int() || refI(ma.opnd0().tipo()).es_real()) &&
+                (refI(ma.opnd1().tipo()).es_int() || refI(ma.opnd1().tipo()).es_real())) {
+            if (es_desig(ma.opnd0())) {
                 etq++;
             }
-            if (es_designador(ma.opnd1())) {
+            if (es_desig(ma.opnd1())) {
                 etq++;
             }
         }
     }
 
-    public void procesa(Menor me){
+    public void procesa (Menor me){
         me.opnd0().procesa(this);
         me.opnd1().procesa(this);
         etq++;
-        String t0 = me.opnd0().tipo().iden().toString();
-        String t1 = me.opnd1().tipo().iden().toString();
-        if ((t0.equals("int") && t1.equals("real"))) {
-            if (es_designador(me.opnd0())) {
+        if ((refI(me.opnd0().tipo()).es_int() || refI(me.opnd0().tipo()).es_real()) &&
+                (refI(me.opnd1().tipo()).es_int() || refI(me.opnd1().tipo()).es_real())) {
+            if (es_desig(me.opnd0())) {
                 etq++;
             }
-            if (es_designador(me.opnd1())) {
+            if (es_desig(me.opnd1())) {
                 etq++;
             }
         }
     }
 
-    public void procesa(MayorIg mai){
+    public void procesa (MayorIg mai){
         mai.opnd0().procesa(this);
         mai.opnd1().procesa(this);
         etq++;
-        String t0 = mai.opnd0().tipo().iden().toString();
-        String t1 = mai.opnd1().tipo().iden().toString();
-        if ((t0.equals("int") && t1.equals("real"))) {
-            if (es_designador(mai.opnd0())) {
+        if ((refI(mai.opnd0().tipo()).es_int() || refI(mai.opnd0().tipo()).es_real()) &&
+                (refI(mai.opnd1().tipo()).es_int() || refI(mai.opnd1().tipo()).es_real())) {
+            if (es_desig(mai.opnd0())) {
                 etq++;
             }
-            if (es_designador(mai.opnd1())) {
+            if (es_desig(mai.opnd1())) {
                 etq++;
             }
         }
     }
 
-    public void procesa(MenorIg mei){
+    public void procesa (MenorIg mei){
         mei.opnd0().procesa(this);
         mei.opnd1().procesa(this);
         etq++;
-        String t0 = mei.opnd0().tipo().iden().toString();
-        String t1 = mei.opnd1().tipo().iden().toString();
-        if ((t0.equals("int") && t1.equals("real"))) {
-            if (es_designador(mei.opnd0())) {
+        if ((refI(mei.opnd0().tipo()).es_int() || refI(mei.opnd0().tipo()).es_real()) &&
+                (refI(mei.opnd1().tipo()).es_int() || refI(mei.opnd1().tipo()).es_real())) {
+            if (es_desig(mei.opnd0())) {
                 etq++;
             }
-            if (es_designador(mei.opnd1())) {
+            if (es_desig(mei.opnd1())) {
                 etq++;
             }
         }
     }
 
-    public void procesa(Igual ig){
+    public void procesa (Igual ig){
         ig.opnd0().procesa(this);
         ig.opnd1().procesa(this);
         etq++;
-        String t0 = ig.opnd0().tipo().iden().toString();
-        String t1 = ig.opnd1().tipo().iden().toString();
-        if ((t0.equals("int") && t1.equals("real"))) {
-            if (es_designador(ig.opnd0())) {
+        if ((refI(ig.opnd0().tipo()).es_int() || refI(ig.opnd0().tipo()).es_real()) &&
+                (refI(ig.opnd1().tipo()).es_int() || refI(ig.opnd1().tipo()).es_real())) {
+            if (es_desig(ig.opnd0())) {
                 etq++;
             }
-            if (es_designador(ig.opnd1())) {
+            if (es_desig(ig.opnd1())) {
                 etq++;
             }
         }
     }
 
-    public void procesa(Desigual de){
+    public void procesa (Desigual de){
         de.opnd0().procesa(this);
         de.opnd1().procesa(this);
         etq++;
         String t0 = de.opnd0().tipo().iden().toString();
         String t1 = de.opnd1().tipo().iden().toString();
-        if ((t0.equals("int") && t1.equals("real"))) {
-            if (es_designador(de.opnd0())) {
+        if ((refI(de.opnd0().tipo()).es_int() || refI(de.opnd0().tipo()).es_real()) &&
+                (refI(de.opnd1().tipo()).es_int() || refI(de.opnd1().tipo()).es_real())) {
+            if (es_desig(de.opnd0())) {
                 etq++;
             }
-            if (es_designador(de.opnd1())) {
+            if (es_desig(de.opnd1())) {
                 etq++;
             }
         }
     }
 
-    public void procesa(Not no){
+    public void procesa (Not no){
         no.opnd0().procesa(this);
         etq++;
-        String t0 = no.opnd0().tipo().iden().toString();
-        if ((t0.equals("int") || t0.equals("real"))) {
-            if (es_designador(no.opnd0())) {
+        if (refI(no.opnd0().tipo()).es_int() || refI(no.opnd0().tipo()).es_real() {
+            if (es_desig(no.opnd0())) {
                 etq++;
             }
         }
     }
 
-    public void procesa(Lit_bool l){
+    public void procesa (Lit_bool l){
         //NOOP
     }
 
-    public void procesa(Lit_ent l){
+    public void procesa (Lit_ent l){
         //NOOP
     }
 
-    public void procesa(Lit_string l){
+    public void procesa (Lit_string l){
         //NOOP
     }
 
-    public void procesa(Lit_real l){
+    public void procesa (Lit_real l){
         //NOOP
     }
 
-    public void procesa(Iden i){
+    public void procesa (Iden i){
         //NOOP
     }
-    public void procesa(Struct s){
+    public void procesa (Struct s){
         s.lcamp().procesa(this);
     }
-    public void procesa(Array a){
+    public void procesa (Array a){
         a.tipo().procesa(this);
     }
-    public void procesa(Puntero p) {
+    public void procesa (Puntero p){
 
     }
 
+    //FUNCIONES AUX PARA EXP
+    private Tipo refI(Tipo t){
+        while (t.es_iden()) {
+            Iden id = (Iden) t;
+            Dec_tipo d = (Dec_tipo) id.getVinculo();
+            t = d.tipo();
+        }
+        return t;
+    }
+
+    private boolean es_desig (Exp e){
+        if (e.es_iden()) {
+            return true;
+        } else if (e.prioridad() == 6) { //Prioridad 5 para los accesos.
+            return true;
+        }
+        return false;
+    }
+
+    private int desp (LCamp lcs, String c){
+        while (lcs.es_muchos_campos()) {
+            Camp campo = lcs.campo();
+            if (c.equals(campo.iden().toString())) {
+                return campo.get_desp();
+            }
+            lcs = lcs.lcs();
+        }
+        Camp campo = lcs.campo();
+        return campo.get_desp();
+    }
 }
