f70f460ee50e64869fbdbc63d428f15776fcf982
pali509 <pablcamp@ucm.es>
2024-05-12 23:21:44 +0200

    metido cup bien(?)

--- Fase4/src/DomJudge.java
+++ Fase4/src/DomJudge.java
@@ -30,7 +30,7 @@ private static Prog construye_ast(Reader input, char constructor) throws Excepti
 				// directamente la clase ConstructorASTTiny, en lugar de su especializaci�n
 				// ConstructorASTTinyDJ, e invocamos a parse, en lugar de debug_parse.
 				ConstructorASTTiny asint = new ConstructorASTTiny(alex);
-				Prog p = (Prog)asint.parse().value;
+				Prog p = (Prog) asint.parse().value;
 				return p;
 		}
 		catch(ErrorLexico e) {
