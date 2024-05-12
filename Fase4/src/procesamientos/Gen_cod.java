08b5c4159b2959464b929fd842b9c6f8b6e505d4
José Antonio Ruiz Heredia <56451051+JoseRuiz01@users.noreply.github.com>
2024-05-12 23:26:47 +0200

    ins_call

--- Fase4/src/procesamientos/Gen_cod.java
+++ Fase4/src/procesamientos/Gen_cod.java
@@ -109,7 +109,7 @@ public class Gen_cod extends ProcesamientoDef {
 	public void procesa(Ins_call i){
 		i.pr().procesa(this);
 		Dec_proc dec = (Dec_proc) i.getVinculo();
-		gen_pforms(dec);
+		gen_pforms(dec, i.pr());
 
 		/*
 		let id = string.vinculo, id.vinculo = dec_proc(_, PFormOpt , _) in
@@ -785,27 +785,56 @@ public class Gen_cod extends ProcesamientoDef {
 		recolecta_procs(ldec.dec());
 	}
 
-	private void gen_pforms(Dec_proc dec){
-		if(dec.pf().es_no_pform()) {
-			No_pforms pf = (No_pforms) dec.pf();
-		}
-		else if(dec.pf().pforms().es_un_pform()) {
+	private void gen_pforms(Dec_proc dec, LPRealOpt pr){
+		if(dec.pf().pforms().es_un_pform() && pr.lpr().es_un_preal()) {
 			Un_pform unpf = (Un_pform) dec.pf().pforms();
-			gen_pforms(unpf.pform());
+			Un_PReal unpr = (Un_PReal) pr.lpr();
+			gen_pforms(unpf.pform(), unpr.lpr());
 		}
-		else if(dec.pf().pforms().es_muchos_pform() ) {
+		else if(dec.pf().pforms().es_muchos_pform() && pr.lpr().es_muchos_preal()) {
 			Muchos_pforms mpf = (Muchos_pforms) dec.pf().pforms();
-			gen_pforms(mpf.pforms());
-			gen_pforms(mpf.pform());
+			Muchos_preal mpr = (Muchos_preal) pr.lpr();
+			gen_pforms(mpf.pforms(), mpr.lpr());
+			gen_pforms(mpf.pform(), mpr.lpr());
 		}
 	}
-	private void gen_pforms(Pform pf) {
-
+	private void gen_pforms(Pform pf, LPReal lpr) {
+		lpr.procesa(this);
+		mp.emit(mp.alloc(lpr.e().tipo().getTam()));
+		if((refI(lpr.e().tipo()).es_int())){
+			if(es_desig(lpr.e())) {
+				mp.emit(mp.fetch(lpr.e().get_dir()));
+			}
+		}
+		else{
+			if(es_desig(lpr.e())) {
+				mp.emit(mp.copia(lpr.e().tipo().getTam()));
+			}
+			else{
+				mp.emit(mp.store_real(lpr.e().get_dir(), Double.parseDouble(lpr.e().valor().toString())));
+			}
+		}
 	}
 
-	private void gen_pforms(LPForm lpf) {
-		gen_pforms(lpf.pforms());
-		gen_pforms(lpf.pform());
+	private void gen_pforms(LPForm lpf,  LPReal lpr) {
+		gen_pforms(lpf.pforms(), lpr.lpr());
+		gen_pforms(lpf.pform(), lpr.lpr());
 	}
 
+	/*
+	EL ENUNCIADO:
+		Para cada parámetro formal con nombre u, si ya existe una variable u, debe
+		salvaguardarse dir(u).
+		o Para cada parámetro formal en modo valor ti ui:
+			▪ dir(ui)  alloc(ti)
+			▪ Evaluar el correspondiente parámetro real Ei. Sea tRi el tipo de Ei y sea ri el
+		resultado de la evaluación:
+		• Si ti es real y tRi es int:
+			o Si Ei es un designador, vi  fetch(ri). Si no, vi  ri
+			o Convertir vi a real. Sea v’i el resultado de dicha conversión
+			o store(dir(ui), v’i)
+		• en otro caso:
+			o Si Ei es un designador, copy(dir(ui), ri, i)
+			o En otro caso, store(dir(ui), ri)
+	 */
 }
